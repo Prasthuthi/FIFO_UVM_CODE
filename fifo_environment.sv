c63dc4ef9f7d9e19f38b5f9fb1f09bcb8a8b9c18
Prasthuthi <126679863+Prasthuthi@users.noreply.github.com>
2023-09-20 19:29:29 +0530

    Update and rename fifo_env.sv to fifo_environment.sv

--- fifo_environment.sv
+++ fifo_environment.sv
@@ -1,13 +1,14 @@
-//environment
- `include "fifo_agent.sv"
+`include "fifo_agent.sv"
 `include "fifo_scoreboard.sv"
+`include "fifo_coverage.sv"
 
-class fifo_env extends uvm_env;
+class fifo_environment extends uvm_env;
   fifo_agent f_agt;
   fifo_scoreboard f_scb;
-  `uvm_component_utils(fifo_env)
+  fifo_coverage f_cov;
+  `uvm_component_utils(fifo_environment)
   
-  function new(string name = "fifo_env", uvm_component parent);
+  function new(string name = "fifo_environment", uvm_component parent);
     super.new(name, parent);
   endfunction
   
@@ -15,10 +16,12 @@ class fifo_env extends uvm_env;
     super.build_phase(phase);
     f_agt = fifo_agent::type_id::create("f_agt", this);
     f_scb = fifo_scoreboard::type_id::create("f_scb", this);
+     f_cov = fifo_coverage::type_id::create("f_cov",this);
   endfunction
   
   virtual function void connect_phase(uvm_phase phase);
     f_agt.f_mon.item_got_port.connect(f_scb.item_got_export);
+    f_agt.f_mon.item_got_port.connect(f_cov.item_got_export_sub);
   endfunction
   
 endclass
