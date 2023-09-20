629bbc06dc5a8f715331295984a7a9050c3f66ad
Prasthuthi <126679863+Prasthuthi@users.noreply.github.com>
2023-09-20 19:30:38 +0530

    Update fifo_interface.sv

--- fifo_interface.sv
+++ fifo_interface.sv
@@ -1,4 +1,3 @@
-//interface
 import uvm_pkg::*;
 `include "uvm_macros.svh"
 `timescale 1ns/10ps
@@ -14,26 +13,26 @@ interface fifo_interface(input clk, rstn);
   bit [127:0] o_rddata;
   
   clocking dri_cb @(posedge clk);
-    default input #1 output #0;
+    default input #1 output #1;
     output i_wren;
     output i_rden;
     output i_wrdata;
   endclocking
   
   clocking mon_cb @(posedge clk);
-    default input #1 output #0;
+    default input #1 output #1;
+    input o_full;
+    input o_empty;
+     input o_alm_full;
+  input o_alm_empty;
+    input o_rddata;
     input i_wren;
     input i_rden;
     input i_wrdata;
-    input o_full;
-    input o_empty;
-    input o_alm_full;
-    input o_alm_empty;
-    input o_rddata;
   endclocking
-  
+
   modport dri_mp (input clk, rstn, clocking dri_cb);
-    modport mon_mp (input clk, rstn, clocking mon_cb);
+  
+      modport mon_mp (input clk, rstn, clocking mon_cb);
     
 endinterface
-    
