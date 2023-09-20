c94b60a4abca7156c42edce152746dcf2f377b3a
Prasthuthi <126679863+Prasthuthi@users.noreply.github.com>
2023-09-20 19:27:20 +0530

    Update fifo_agent.sv

--- fifo_agent.sv
+++ fifo_agent.sv
@@ -1,18 +1,13 @@
-    
-    //agent
- `include "fifo_seq_item.sv"
+`include "fifo_seq_item.sv"
 `include "fifo_sequence.sv"
 `include "fifo_sequencer.sv"
 `include "fifo_driver.sv"
-`include "fifo_monitor.sv"   
-    
-
+`include "fifo_monitor.sv"
 
 class fifo_agent extends uvm_agent;
- fifo_sequencer f_seqr;
-  fifo_driver  f_dri;
+  fifo_sequencer f_seqr;
+  fifo_driver f_dri;
   fifo_monitor f_mon;
-  
   `uvm_component_utils(fifo_agent)
   
   function new(string name = "fifo_agent", uvm_component parent);
@@ -22,8 +17,8 @@ class fifo_agent extends uvm_agent;
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     if(get_is_active() == UVM_ACTIVE) begin
-      f_seqr = fifo_sequencer ::type_id::create("f_seqr", this);
-      f_dri = fifo_driver ::type_id::create("f_dri", this);
+      f_seqr = fifo_sequencer::type_id::create("f_seqr", this);
+      f_dri = fifo_driver::type_id::create("f_dri", this);
     end
     f_mon = fifo_monitor::type_id::create("f_mon", this);
   endfunction
@@ -34,3 +29,5 @@ class fifo_agent extends uvm_agent;
   endfunction
   
 endclass
+    
+    
